8981d0e7c3c733cae10358403164c1a09dc3831c
Pedro Monteiro <83165668+pedroafmonteiro@users.noreply.github.com>
2025-03-21 09:28:34 +0000

    Add tabbing.

--- menu/menu.cpp
+++ menu/menu.cpp
@@ -10,12 +10,12 @@ Menu::Menu() {
     std::cout << "" << std::endl;
     std::cout << "Please select an option:" << std::endl;
     std::cout << "" << std::endl;
-    std::cout << "0. Load dataset." << std::endl;
+    std::cout << "  0. Load dataset." << std::endl;
     std::cout << "" << std::endl;
-    std::cout << "1. Independent Route. Best (fastest) route between a source and destination." << std::endl;
-    std::cout << "2. Restricted Route. Fastest route with specific routing restrictions." << std::endl;
-    std::cout << "3. Environmentally-Friendly Route. Best (shortest overall) route for driving and walking." << std::endl;
+    std::cout << "  1. Independent Route. Best (fastest) route between a source and destination." << std::endl;
+    std::cout << "  2. Restricted Route. Fastest route with specific routing restrictions." << std::endl;
+    std::cout << "  3. Environmentally-Friendly Route. Best (shortest overall) route for driving and walking." << std::endl;
     std::cout << "" << std::endl;
-    std::cout << "4. Exit." << std::endl;
+    std::cout << "  4. Exit." << std::endl;
     std::cin >> option;
 }
